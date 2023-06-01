bb869785738fcb17d55503865ef4f009689a87d6
Alecs <erick.borges@outlook.es>
2023-06-01 15:42:56 -0600

    Video Screen

--- Routes.js
+++ Routes.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from './screens/HomeScreen';
 import WorkoutDetail from './screens/WorkoutDetail';
+import VideoScreen from './screens/VideoScreen';
 
 
 const StackNavigator = () => {
@@ -13,6 +14,7 @@ const StackNavigator = () => {
         <Stack.Navigator>
             <Stack.Screen name='Home' component={HomeScreen} options={{ headerShown: false }}/>
             <Stack.Screen name='WorkoutDetail' component={WorkoutDetail} options={{ headerShown: false }}/>
+            <Stack.Screen name='Video' component={VideoScreen} options={{ headerShown: false }}/>
         </Stack.Navigator>
     </NavigationContainer>
   )
